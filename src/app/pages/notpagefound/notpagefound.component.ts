ee8abba0be8b9597a4eb209fcef32020588e5797
cruzteodoro <cruzteodoromp@gmail.com>
2020-08-14 14:31:28 -0500

    Fin seccion 3 - Diseño

---
+++ src/app/pages/notpagefound/notpagefound.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notpagefound',
+  templateUrl: './notpagefound.component.html',
+  styleUrls:[
+    './notpagefound.component.css'
+  ],
+  styles: [
+  ]
+})
+export class NotpagefoundComponent {
+
+  year=new Date().getFullYear();
+
+  
+
+}
